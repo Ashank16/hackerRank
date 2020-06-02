e6ddf85cc7c44d1f674a110cd8df0f5b8f53804b
Ashank <54265111+Ashank16@users.noreply.github.com>
2020-06-02 18:45:50 +0530

    Create Day1.cpp

---
+++ Day1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    // Declare second integer, double, and String variables.
+    int i1;
+    double d1;
+    string s1;
+    
+    // Read and save an integer, double, and String to your variables.    
+    cin>>i1;    
+    cin>>d1;
+    cin.ignore(); //to ignore whitespace for getline function    
+    getline(cin,s1);
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    cout<<i + i1<<endl;
+    // Print the sum of the double variables on a new line.
+    cout<<fixed<<setprecision(1)<<d + d1<<endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout<<s + s1<<endl;
+
+    return 0;
+}
